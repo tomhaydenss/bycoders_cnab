938b23c21971c37ed621915c8edb35c299b316c5
Tom Hayden <tomhaydenss@gmail.com>
2022-06-15 02:07:27 -0300

    Add frontend client for file uploading

--- assets/js/Home.jsx
+++ assets/js/Home.jsx
@@ -1,13 +1,33 @@
 import React from "react";
+import { useMutation, gql } from '@apollo/client'
 
-const Home = () => {
+const UPLOAD_FILE = gql`
+  mutation UploadFile($file: Upload!) {
+    uploadFile(file: $file)
+  }
+`
+
+export default function Home() {
+  const [uploadFile] = useMutation(UPLOAD_FILE);
+
+  function onChange({
+    target: {
+      validity,
+      files: [file],
+    },
+  }) {
+    if (validity.valid) {
+      uploadFile({ variables: { file } });
+    }
+  }
 
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <p>Welcome to Phoenix with TypeScript and React</p>
+    <div className="app">
+      <h1>CNAB File Parsing</h1>
+      <div className="file-upload">
+        <h3>Choose a CNAB file for upload</h3>
+        <input type="file" required onChange={onChange} />
+      </div>
     </div>
-  );
-};
-
-export default Home;
+  )
+}
--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -6,9 +6,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ApolloClient, ApolloProvider } from "@apollo/client";
 import { InMemoryCache } from "@apollo/client/cache";
+import { createLink } from "apollo-absinthe-upload-link";
 import Home from "./Home";
 
 const client = new ApolloClient({
+    link: createLink({
+        uri: "/api/graphql"
+    }),
     uri: "/api/graphql",
     cache: new InMemoryCache(),
 });
